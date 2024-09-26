a1a9f78790e698d1299e31d2a839fabd4af6d94c
hunghx <baby10051999@gmail.com>
2024-09-26 10:54:36 +0700

    up

--- src/main/java/ra/orm/controller/HomeController.java
+++ src/main/java/ra/orm/controller/HomeController.java
@@ -1,36 +1,46 @@
 package ra.orm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import ra.orm.model.FormRequest;
-import ra.orm.validate.FormRequestValidate;
 
-import javax.validation.Valid;
-import java.util.ArrayList;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
     @Autowired
-    private FormRequestValidate formRequestValidate;
-@GetMapping
-    public String home(Model model) {
-        FormRequest init = new FormRequest();
-        model.addAttribute("formrequest", init);
-        return "form";
+    private HttpSession session;
+    @GetMapping
+    public String count(@CookieValue(value = "count",defaultValue = "0") Integer count,
+                        HttpServletResponse response, Model model){
+        // B1 : lấy ra số lần truy cập trang chủ ở cookies của trình duyệt
+        // B2 : tăng sô lần leen 1
+        count++;
+        // cập nhật count trong cookies
+        Cookie cookie = new Cookie("count",count.toString());
+        cookie.setMaxAge(3600);
+        response.addCookie(cookie);
+        model.addAttribute("count",count);
+        return "home";
     }
-    @PostMapping("/validate")
-    public String validate(@ModelAttribute("formrequest") FormRequest formRequest, BindingResult result,Model model) {
-    formRequestValidate.validate(formRequest,result);
-    if (result.hasErrors()) {
-            model.addAttribute("formrequest", formRequest);
-            return "form";
-        }else {
-            return "home";
+    @GetMapping("/count")
+    private String counter(Model model){
+        // B1 lấy ra count hiện tại
+        Object obj = session.getAttribute("count");
+        Integer count = 0; // giá trị mặc định
+        if (obj != null ){
+            count = (Integer) obj;
         }
+        count++;
+        // lưu count lại trong session và ửi về phía giao dienj ng dùng
+        session.setAttribute("count",count);
+        model.addAttribute("count",count);
+        return "count";
     }
 }
